c4d790383cd78d21d7c74513f9f613a5c960947f
Jayavenkate <jayajaikithuja@gmail.com>
2023-04-30 22:02:00 +0300

    vercel

--- index.js
+++ index.js
@@ -11,9 +11,8 @@ import { MongoClient } from "mongodb";
 const app = express();
 
 const PORT = process.env.PORT;
-// 
+//
 const MONGO_URL = process.env.MONGO_URL;
-
 
 export const client = new MongoClient(MONGO_URL);
 await client.connect();
@@ -21,21 +20,21 @@ console.log("Mongo is connected !!!  ");
 
 app.use(express.json());
 app.use(cors());
-app.use("/",pizzaRouter);
-app.use("/",usersRourter);
+app.use("/", pizzaRouter);
+app.use("/", usersRourter);
 // const pizzalist = [
 //     {
 //       name: "Pepper Barbecue Chicken",
 //       prices: 350,
-  
+
 //       image: "https://www.dominos.co.in//files/items/Pepper_Barbeque_&_Onion.jpg",
 //       description: "Pepper Barbecue Chicken I Cheese",
 //     },
 //     {
 //       name: "Non Veg Supreme",
-  
+
 //       prices: 400,
-  
+
 //       image:
 //         "https://www.dominos.co.in/theme2/front/images/menu-images/my-nonveg.webp",
 //       description:
@@ -43,27 +42,27 @@ app.use("/",usersRourter);
 //     },
 //     {
 //       name: "Golden Corn Pizza",
-  
+
 //       prices: 350,
-  
+
 //       image: "https://www.dominos.co.in//files/items/Corn_&_Cheese.jpg",
 //       description:
 //         "Regardless of how you're preparing it, corn adds a fresh, sweet crunch to mealtime that just can't be replicated by anything else. And it's that inimitable burst of crunchy sweetness that makes corn the perfect pizza topping too",
 //     },
 //     {
 //       name: " Red Paprika Pizza",
-  
+
 //       prices: 200,
-  
+
 //       image: "https://www.dominos.co.in//files/items/5_Pepper.jpg",
 //       description:
 //         "Fresh, tender and deep red color ideal for grilled and roasted cuisines to enhance the extra punch in veg and non veg food. Red paprika is used on pizzas, sandwiches, wraps, etc, it is wonderful on nachos, crackers with cheese and for making chilly cheese toast.",
 //     },
 //     {
 //       name: "Margerita",
-  
+
 //       prices: 400,
-  
+
 //       image:
 //         "https://cdn.loveandlemons.com/wp-content/uploads/2019/09/margherita-pizza.jpg",
 //       description:
@@ -71,9 +70,9 @@ app.use("/",usersRourter);
 //     },
 //     {
 //       name: " Cheese Margerita Pizza ",
-  
+
 //       prices: 350,
-  
+
 //       image:
 //         "https://www.dominos.co.in//files/items/Double_Cheese_Margherita.jpg",
 //       description:
@@ -81,9 +80,9 @@ app.use("/",usersRourter);
 //     },
 //     {
 //       name: " Pepperoni ",
-  
+
 //       prices: 450,
-  
+
 //       image:
 //         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXTHEEdwoBf83QnlIkJlBLrmqGyOr-yZKobQ&usqp=CAU",
 //       description:
@@ -91,9 +90,9 @@ app.use("/",usersRourter);
 //     },
 //     {
 //       name: " Hawaiian ",
-  
+
 //       prices: 450,
-  
+
 //       image:
 //         "https://www.kingarthurbaking.com/sites/default/files/styles/featured_image/public/2020-03/hawaiian-pizza.jpg?itok=a1-_QjRA",
 //       description:
@@ -101,9 +100,9 @@ app.use("/",usersRourter);
 //     },
 //     {
 //       name: " Meat Lovers ",
-  
+
 //       prices: 450,
-  
+
 //       image:
 //         "https://emeals-menubuilder.s3.amazonaws.com/v1/recipes/781180/pictures/large_meat-lovers-pizza.jpeg",
 //       description:
@@ -111,9 +110,9 @@ app.use("/",usersRourter);
 //     },
 //     {
 //       name: " Vegetarian ",
-  
+
 //       prices: 450,
-  
+
 //       image:
 //         "https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/1200x1200/Whole-Wheat-Veggie-Pizza_EXPS_HCKA19_12558_C10_13_5b.jpg",
 //       description:
@@ -121,9 +120,9 @@ app.use("/",usersRourter);
 //     },
 //     {
 //       name: " Buffalo Chicken ",
-  
+
 //       prices: 450,
-  
+
 //       image:
 //         "https://easychickenrecipes.com/wp-content/uploads/2019/11/buffalo-chicken-pizza-4.jpg",
 //       description:
@@ -131,9 +130,9 @@ app.use("/",usersRourter);
 //     },
 //     {
 //       name: " Spinach and Feta ",
-  
+
 //       prices: 450,
-  
+
 //       image:
 //         "https://thelittlestcrumb.com/wp-content/uploads/spinach-and-feta-pizza-featured-image.jpg",
 //       description:
@@ -141,9 +140,9 @@ app.use("/",usersRourter);
 //     },
 //     {
 //       name: " Four Cheese ",
-  
+
 //       prices: 450,
-  
+
 //       image:
 //         "https://www.insidetherustickitchen.com/wp-content/uploads/2020/07/Quattro-formaggi-pizza-square-Inside-the-rustic-kitchen.jpg",
 //       description:
@@ -154,8 +153,6 @@ app.get("/", function (request, response) {
   response.send("🙋‍♂️, 🌏 🎊✨🤩");
 });
 
-
 app.listen(PORT, () => console.log(`The server started in: ${PORT} ✨✨`));
 
 //generate hashed password
-
